7e03f05029036e6888ac45535c27ee87fea29dd5
ef10007 <jinseon-han@hotmail.com>
2018-11-16 11:35:43 +0900

    person

--- portfolio/blackjack1.py
+++ portfolio/blackjack1.py
@@ -1,5 +1,9 @@
 import random
 
-dcard = []
-pcard = []
+d_cards = []
+p_cards = []
 
+while len(dcards) != 2:
+    dcards.append(range(1,52))
+    if len(dcards) == 2:
+        print ("Dealer has" ,)
